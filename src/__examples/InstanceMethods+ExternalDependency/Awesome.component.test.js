9d4dbdd4e5b11ec0623094017152d7548ffd8d88
Slavomir Vojacek <slavomirvojacek@gmail.com>
2017-07-03 18:08:59 +0700

    Preferred way to mock external deps / libs

--- src/__examples/InstanceMethods+ExternalDependency/Awesome.component.test.js
+++ src/__examples/InstanceMethods+ExternalDependency/Awesome.component.test.js
@@ -1,25 +1,25 @@
 import React from 'react'
 
 import { AwesomeComponent } from './Awesome.component'
+
 import SomethingExternal from './lib/SomethingExternal'
 import SomethingElseExternal from './lib/SomethingElseExternal'
 
 const getInstance = p => new AwesomeComponent(p)
 
 jest
+  .mock('./lib/SomethingExternal', () => ({
+    awesomeMethod: jest.fn()
+  }))
   .mock('./lib/SomethingElseExternal', () => ({
     anotherAwesomeMethod: jest.fn()
   }))
 
 describe('AwesomeComponent', () => {
-  let props, awesomeMethod
+  let props
 
   beforeEach(() => {
     props = {...AwesomeComponent.defaultProps, logIntoConsole: jest.fn()}
-
-    awesomeMethod = jest
-      .spyOn(SomethingExternal, 'awesomeMethod')
-      .mockImplementation(jest.fn())
   })
 
   describe('instance', () => {
@@ -32,7 +32,7 @@ describe('AwesomeComponent', () => {
     test('someHandler calls SomethingExternal.awesomeMethod and SomethingElseExternal.awesomeMethod with correct argument', () => {
       let subject = getInstance(props)
       subject.someHandler('val')
-      expect(awesomeMethod).toHaveBeenCalledWith('val')
+      expect(SomethingExternal.awesomeMethod).toHaveBeenCalledWith('val')
       expect(SomethingElseExternal.anotherAwesomeMethod).toHaveBeenCalledWith('val')
     })
   })
